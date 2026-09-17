42f9a00d4c6c94b19cbb255f97801f302b4fa82f
wadezigh96 <xyzkames@gmail.com>
2026-09-17 22:47:14 +0700

    fix: simplify Privy provider configuration

--- components/PrivyProvider.tsx
+++ components/PrivyProvider.tsx
@@ -5,16 +5,5 @@ import { PrivyProvider as Provider } from '@privy-io/react-auth'
 export default function PrivyProvider({ children }: { children: React.ReactNode }) {
   const appId = process.env.NEXT_PUBLIC_PRIVY_APP_ID
   if (!appId) return <>{children}</>
-
-  return (
-    <Provider
-      appId={appId}
-      config={{
-        supportedChains: [{ id: 8453, name: 'Base', nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 }, rpcUrls: { default: { http: ['https://mainnet.base.org'] }, public: { http: ['https://mainnet.base.org'] } }, blockExplorers: { default: { name: 'BaseScan', url: 'https://basescan.org' } } }] as any,
-        defaultChain: { id: 8453, name: 'Base', nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 }, rpcUrls: { default: { http: ['https://mainnet.base.org'] }, public: { http: ['https://mainnet.base.org'] } }, blockExplorers: { default: { name: 'BaseScan', url: 'https://basescan.org' } } } as any,
-      }}
-    >
-      {children}
-    </Provider>
-  )
+  return <Provider appId={appId}>{children}</Provider>
 }
